b1a4b8a88ec8e5313ee6d2ae3ef75225398a3805
santosjunior1403-bit <santosjunior.1403@gmail.com>
2026-06-11 14:57:39 -0300

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const gaId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+if (gaId) {
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${gaId}');
+  `;
+  document.head.appendChild(script2);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
