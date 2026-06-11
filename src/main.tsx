d1461d39fe02e9b6912a688e80002b435cde03cd
santosjunior1403-bit <santosjunior.1403@gmail.com>
2026-06-11 16:02:34 -0300

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -9,21 +9,4 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 );
 
-const gaId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
-if (gaId) {
-  // Inject Google Analytics
-  const scriptTag = document.createElement("script");
-  scriptTag.async = true;
-  scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-  document.head.appendChild(scriptTag);
-
-  // Initialize dataLayer safely
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = function(...args: any[]) {
-    window.dataLayer.push(args);
-  };
-  
-  window.gtag('js', new Date());
-  window.gtag('config', gaId);
-}
